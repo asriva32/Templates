7ea003894ef909dfee7cb4a4955f20e09c8ffd9d
Arnav Srivastava <arnavsrivastava@Arnavs-MacBook-Pro-5.local>
2024-10-08 21:43:44 -0400

    change

--- articulation_point.cpp
+++ articulation_point.cpp
@@ -3,14 +3,14 @@ vector<int> dfs_num(mxN, -1), dfs_low(mxN)
 vector<int> dfs_parent(mxN, -1), articulation_vertex(mxN);
 int dfsNumberCounter, dfsRoot, rootChildren;
 // O(V + E)
-void find_bridges(int u) {
+void find_ap(int u) {
     dfs_num[u] = dfsNumberCounter++;
     dfs_low[u] = dfs_num[u];
     for (int v : adj[u]) {
         if (dfs_num[v] == -1) {
             dfs_parent[v] = u;
             if (u == dfsRoot) ++rootChildren;
-            find_bridges(v);
+            find_ap(v);
             if (dfs_low[v] >= dfs_num[u])
                 articulation_vertex[u] = 1;
             if (dfs_low[v] > dfs_num[u])
